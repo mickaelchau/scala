9bd90f1cbad6d822845e3c8848d6bf7d48062d2a
Jim Newton <jnewton@lrde.epita.fr>
2022-02-01 15:28:42 +0100

    rsync

--- src/test/scala/AdjStdLibTestSuite.scala
+++ src/test/scala/AdjStdLibTestSuite.scala
@@ -28,19 +28,19 @@ class AdjStdLibTestSuite extends AnyFunSuite {
     assert(reversedEdges(List[(Int,Int)]()).toList == List())
     assert(reversedEdges(List((1,2))).toList == List((2,1)))
     assert(reversedEdges(List((1,2),(3,4))).toList == List((2,1),(4,3)))
-    assert(reversedEdges(Array[(Int,Int)]()).toList == List())
-    assert(reversedEdges(Array((1,2))).toList == List((2,1)))
-    assert(reversedEdges(Array((1,2),(3,4))).toList == List((2,1),(4,3)))
+    assert(reversedEdges(Vector[(Int,Int)]()).toList == List())
+    assert(reversedEdges(Vector((1,2))).toList == List((2,1)))
+    assert(reversedEdges(Vector((1,2),(3,4))).toList == List((2,1),(4,3)))
     assert(reversedEdges(Seq[(Int,Int)]()).toList == List())
     assert(reversedEdges(Seq((1,2))).toList == List((2,1)))
     assert(reversedEdges(Seq((1,2),(3,4))).toList == List((2,1),(4,3)))
   }
 
   test("building adjacency list") {
-    assert(makeAdj(Array((1, 2)), false).get(2).contains(Set(1)))
-    assert(makeAdj(Array((1, 2)), true).get(2).isEmpty)
+    assert(makeAdj(Vector((1, 2)), false).get(2).contains(Set(1)))
+    assert(makeAdj(Vector((1, 2)), true).get(2).isEmpty)
 
-    assert(makeAdj(Array((1,2)),false)
+    assert(makeAdj(Vector((1,2)),false)
              == Map(1 -> Set(2),
                     2 -> Set(1)))
     assert(makeAdj(List((1,2),(2,3)), false)
@@ -52,26 +52,26 @@ class AdjStdLibTestSuite extends AnyFunSuite {
                     "b" -> Set("c","d")))
   }
 
-  test("collect connected vertices by Array") {
-    assert(reachableVertices(Array((1, 2), (3, 4), (4, 2)), 1, false) == Set(1, 2, 3, 4))
-    assert(reachableVertices(Array((1, 2), (3, 4), (4, 2)), 2, false) == Set(1, 2, 3, 4), "undirected")
-    assert(reachableVertices(Array((1, 2), (3, 4), (4, 2)), 3, false) == Set(1, 2, 3, 4))
-    assert(reachableVertices(Array((1, 2), (3, 4), (4, 2)), 4, false) == Set(1, 2, 3, 4))
-    assert(reachableVertices(Array((1, 2), (3, 4), (4, 2)), 1, true) == Set(1, 2))
-    assert(reachableVertices(Array((1, 2), (3, 4), (4, 2)), 2, true) == Set(2), "directed")
-    assert(reachableVertices(Array((1, 2), (3, 4), (4, 2)), 3, true) == Set(3, 4, 2))
-    assert(reachableVertices(Array((1, 2), (3, 4), (4, 2)), 4, true) == Set(4, 2))
+  test("collect connected vertices by Vector") {
+    assert(reachableVertices(Vector((1, 2), (3, 4), (4, 2)), 1, false) == Set(1, 2, 3, 4))
+    assert(reachableVertices(Vector((1, 2), (3, 4), (4, 2)), 2, false) == Set(1, 2, 3, 4), "undirected")
+    assert(reachableVertices(Vector((1, 2), (3, 4), (4, 2)), 3, false) == Set(1, 2, 3, 4))
+    assert(reachableVertices(Vector((1, 2), (3, 4), (4, 2)), 4, false) == Set(1, 2, 3, 4))
+    assert(reachableVertices(Vector((1, 2), (3, 4), (4, 2)), 1, true) == Set(1, 2))
+    assert(reachableVertices(Vector((1, 2), (3, 4), (4, 2)), 2, true) == Set(2), "directed")
+    assert(reachableVertices(Vector((1, 2), (3, 4), (4, 2)), 3, true) == Set(3, 4, 2))
+    assert(reachableVertices(Vector((1, 2), (3, 4), (4, 2)), 4, true) == Set(4, 2))
   }
-  test("disconnected graph by Array") {
+  test("disconnected graph by Vector") {
     // now a disconnected graph
 
-    assert(reachableVertices(Array((1,2),(1,3),(1,4),(4,1),
+    assert(reachableVertices(Vector((1,2),(1,3),(1,4),(4,1),
                                    (10.1,11),(10.1,12),(11,12)),11,false) == Set(10.1,11,12))
-    assert(reachableVertices(Array((1,2),(1,3),(1,4),(4,1),
+    assert(reachableVertices(Vector((1,2),(1,3),(1,4),(4,1),
                                    (10.1,11),(10.1,12),(11,12)),11,true) == Set(11,12))
-    assert(reachableVertices(Array((1,2),(1,3),(1,4),(4,1),
+    assert(reachableVertices(Vector((1,2),(1,3),(1,4),(4,1),
                                    (10.1,11),(10.1,12),(11,12)),1,false) == Set(1,2,3,4))
-    assert(reachableVertices(Array((1,2),(1,3),(1,4),(4,1),
+    assert(reachableVertices(Vector((1,2),(1,3),(1,4),(4,1),
                                    (10.1,11),(10.1,12),(11,12)),1,true) == Set(1,2,3,4))
   }
 
@@ -122,16 +122,16 @@ class AdjStdLibTestSuite extends AnyFunSuite {
     assert(reachableVertices(List(("rosalie","gilbert"),("rosalie","theophile"),("rosalie","fred"),("fred","rosalie"),
                                   ("germaine","john"),("germaine","emilienne"),("john","emilienne")),"rosalie",true) == Set("rosalie","gilbert","theophile","fred"))
   }
-  test("disconnected graph with Array[String]") {
+  test("disconnected graph with Vector[String]") {
     // now a disconnected graph
 
-    assert(reachableVertices(Array(("rosalie","gilbert"),("rosalie","theophile"),("rosalie","fred"),("fred","rosalie"),
+    assert(reachableVertices(Vector(("rosalie","gilbert"),("rosalie","theophile"),("rosalie","fred"),("fred","rosalie"),
                                   ("germaine","john"),("germaine","emilienne"),("john","emilienne")),"john",false) == Set("germaine","john","emilienne"))
-    assert(reachableVertices(Array(("rosalie","gilbert"),("rosalie","theophile"),("rosalie","fred"),("fred","rosalie"),
+    assert(reachableVertices(Vector(("rosalie","gilbert"),("rosalie","theophile"),("rosalie","fred"),("fred","rosalie"),
                                   ("germaine","john"),("germaine","emilienne"),("john","emilienne")),"john",true) == Set("john","emilienne"))
-    assert(reachableVertices(Array(("rosalie","gilbert"),("rosalie","theophile"),("rosalie","fred"),("fred","rosalie"),
+    assert(reachableVertices(Vector(("rosalie","gilbert"),("rosalie","theophile"),("rosalie","fred"),("fred","rosalie"),
                                   ("germaine","john"),("germaine","emilienne"),("john","emilienne")),"rosalie",false) == Set("rosalie","gilbert","theophile","fred"))
-    assert(reachableVertices(Array(("rosalie","gilbert"),("rosalie","theophile"),("rosalie","fred"),("fred","rosalie"),
+    assert(reachableVertices(Vector(("rosalie","gilbert"),("rosalie","theophile"),("rosalie","fred"),("fred","rosalie"),
                                   ("germaine","john"),("germaine","emilienne"),("john","emilienne")),"rosalie",true) == Set("rosalie","gilbert","theophile","fred"))
   }
 }
